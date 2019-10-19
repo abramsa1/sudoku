b03e27ca2207ad1adb9df261c8138c797939016d
abramsa1 <abramsa1@rambler.ru>
2019-10-19 21:08:30 +0700

    feat: complete Sudouku task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  const gridLength = 9;
+
+  solve(matrix);
+  return matrix;
+
+  function nextCell(grid) {
+    for (let x = 0; x < gridLength; x++) {
+      for (let y = 0; y < gridLength; y++)
+        if (grid[x][y] == 0) return [x, y];
+    }
+    return [-1, -1];
+  }
+
+  function check(grid, i, j, e) {
+    let rowOk = true;
+    for (let x = 0; x < gridLength; x++) {
+      if (e == grid[i][x]) rowOk = false;
+    }
+
+    if (rowOk) {
+      let columnOk = true;
+      for (let x = 0; x < gridLength; x++) {
+        if (e == grid[x][j]) columnOk = false;
+      }
+
+      if (columnOk) {
+        let sectionX = 3 * Math.floor(i / 3);
+        let sectionY = 3 * Math.floor(j / 3);
+
+        for (let x = sectionX; x < sectionX + 3; x++) {
+          for (let y = sectionY; y < sectionY + 3; y++) {
+            if (grid[x][y] == e) return false;
+          }
+        }
+        return true;
+      }
+    }
+    return false
+  }
+
+  function solve(grid) {
+    let temp = nextCell(grid);
+    let i = temp[0]; 
+    let j = temp[1];
+
+    if (i == -1) return true;
+    for (let e = 1; e <= 9; e++) {
+      if (check(grid, i, j, e)) {
+        grid[i][j] = e;
+        if (solve(grid)) return true;
+        grid[i][j] = 0;
+      }
+    }
+    return false;
+  }
 }
